30879408fc219b0509dfa7ab1aa885e0a4239886
Aleks Vino <aleks.vino@gmail.com>
2018-01-19 02:07:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,16 +6,12 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) {
-  console.log(string.toUpperCase)
-  return 
-  function(logShout) {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    return(spy).shout('HELLO')
-
-    console.log.restore()
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === string) {
+      return "I can't hear you!"
+  } else if(string.toUpperCase() === string) {
+      return "YES INDEED!"
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
-
